63dad3f3eafa6bc6f86bb7d5dd4b80bbb66e6cb4
-_- <oncwnuIlKQLRqxN3PWxqW8iETiDo@git.weixin.qq.com>
2020-07-23 17:05:50 +0800

    目录结构划分

---
+++ mall/src/pages/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NavHeader></NavHeader>
+    <rounter-view></rounter-view>
+    <NavFooter></NavFooter>
+  </div>
+</template>
+
+<script>
+import NavHeader from "./components/NavHeader";
+import NavFooter from "./components/NavFooter";
+
+export default {
+  name: "home"
+};
+</script>
+
+<style>
+</style>
